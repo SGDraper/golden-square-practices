5d26035b1cc85d7c61b4a7c24b25dfda5634dd87
Samuel <samuel@seedlegals.com>
2024-02-01 10:25:05 +0000

    Adds password checker tests

--- tests/test_password_checker.py
+++ tests/test_password_checker.py
@@ -1,4 +1,14 @@
+import pytest
 from lib.password_checker import *
 
-def test_over_8():
-    password = PasswordChecker
+def test_under_8():
+    password = PasswordChecker()
+    with pytest.raises(Exception) as e:
+        password.check('four')
+    message = str(e.value)
+    assert message == "Invalid password, must be 8+ characters."
+
+def test_8_or_over():
+    password = PasswordChecker()
+    result = password.check('overeight')
+    assert result == True
